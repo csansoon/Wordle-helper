5926f17ca0e0da2f8f02d4d3bcee739e7bbdf51b
Carlos Sansón <57395395+csansoon@users.noreply.github.com>
2022-02-11 19:22:47 +0100

    Initial commit

---
+++ colors.py
@@ -0,0 +1,18 @@
+from colored import fg, bg, attr
+
+gray = fg('#989898')
+highlight = fg('#FFC648')
+attribute = fg('#2D91FF')
+good = fg('#00D26A')
+bad = fg('#F92F60')
+warning = fg('#F375E2')
+reset = attr(0)
+
+gray_bg = bg('#989898')
+red_bg = bg('#F92F60')
+yellow_bg = bg('#FFC648')
+green_bg = bg('#00D26A')
+gray_fg = fg('#ffffff')
+red_fg = fg('#6A061F')
+yellow_fg = fg('#946500')
+green_fg = fg('#008542')
---
+++ common_word.py
@@ -0,0 +1,3 @@
+from solver import WordleSolver
+solver = WordleSolver()
+print(f'Most common word is ✨{solver.find_best_word()}✨')
